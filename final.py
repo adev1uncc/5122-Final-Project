aa859b98ab1553c210d1a25dcb525525d3424a5e
adev1uncc <116671997+adev1uncc@users.noreply.github.com>
2022-12-10 20:39:07 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,72 +1,61 @@
-from gettext import install
-#from turtle import title
 import streamlit as st
-import random
-import altair as alt
-import numpy as np
 import pandas as pd
-#from vega_datasets import data
+import plotly.express as px
+import altair as alt
 
+
+st.set_page_config(layout = "wide")
+air = pd.read_csv('AIR_EMISSIONS2017 (ENVIRONMENT.AIR_EMISSIONS2017) (ENVIRONMENT)_AIR_EMISSIONS2017.csv')
 st.header('Air Emissions Atlas')
 
-#import data
+st.write()
+
+page = st.sidebar.selectbox('Select a page',
+  ['Introduction','Air Pollution & Causes'])
 
 
-air = pd.read_csv('AIR_EMISSIONS2017 (ENVIRONMENT.AIR_EMISSIONS2017) (ENVIRONMENT)_AIR_EMISSIONS2017.csv')
-st.dataframe(air)
+if page == 'Introduction':
+    st.image("Transport_Graphic_Huge.jpg")
+    st.dataframe(air)
+
+    
+else:
+    contlist = air['Country Name'].unique()
+    country = st.selectbox("Select a Country:",contlist)
+
+
+    st.write('This line chart displays the average values of seven common types of Air Pollution from 1990 to 2018. These types of pollution include Carbon Monoxide, Ammonia(NH3), Nitrogen Oxides, Non-Methane Volatile Organic Compounds, Particulates Pm10 and Pm2.5, and Sulpher Oxides. You can isolate which pollutants you want to focus on on the line chart.')
+
+    df = air.groupby(['Date','Pollutant Name','Country Name'])['Value'].mean().reset_index(name='Value')
+    df['Date'] = pd.DatetimeIndex(df['Date']).year
+    #st.dataframe(f)
+        
+    P_chart = px.line(df[df["Country Name"]==country],
+     x='Date',
+     y='Value',
+     color ='Pollutant Name',
+     title='Types of Air Pollution')
+    st.plotly_chart(P_chart, use_container_width=True)
+
+
+    st.write('This line chart displays the average values of Causes of Air Pollution in the same circumstance as the first line chart. The causes include types of Combustion, Mobile Sources, Agriculture, etc.')
+
+    df1 = air.groupby(['Date','Variable Name','Country Name'])['Value'].mean().reset_index(name='Value')
+    df1['Date'] = pd.DatetimeIndex(df1['Date']).year
+    #st.dataframe(df1)
+
+
+    v = df1[~df1['Variable Name'].isin(['Total Emissions, Index 1990 = 100', 'Total Emissions Per Unit of Gdp, Kg Per 1000 Usd',
+    'Total Emissions, Index 2000 = 100', 'Total Emissions Per Capita','Total Mobile Sources','Total Stationary Sources',
+    'Total Man-Made Emissions'])]
+
+    
+    V_chart = px.line(v[v["Country Name"]==country],
+     x='Date',
+     y='Value',
+     color ='Variable Name',
+     title='Causes of Air Pollution')
+    st.plotly_chart(V_chart, use_container_width=True)
 
 
 
-st.sidebar.header("Pick two variables for the charts")
-x_val = st.sidebar.selectbox("Pick your x-axis",air.select_dtypes(include=np.number).columns.tolist())
-y_val = st.sidebar.selectbox("Pick your y-axis",air.select_dtypes(include=np.number).columns.tolist())
-
-
-
-
-bar = alt.Chart(air, title = f" {x_val} and {y_val} ").mark_bar(fill = 'green',color = 'green').encode(
-    alt.X(x_val, title = f"{x_val}"),
-    alt.Y(y_val, title = f"{y_val}"),
-    tooltip=[x_val,y_val]
-    )
-
-
-st.altair_chart(bar, use_container_width=True)
-
-####
-
-#clist = air['Country Name'].unique()
-#country = st.selectbox("Select a country:",clist)
-#col1, col2 = st.columns(2)
-#air_country = air[air['Country Name']== country]
-#GDP_Chart = alt.Chart(air_country).mark_line().encode(
-#    x = 'Country',
-#    y = 'Scale'
-#)
-#col1.altair_chart(GDP_Chart, use_container_width=True)
-#POP_Chart = alt.Chart(air_country).mark_line().encode(
-#    x = 'Country',
-#    y = 'Scale'
-#)
-#col2.altair_chart(POP_Chart, use_container_width=True)
-
-
-
-# Average Causes of Air Pollution
-
-bar = alt.Chart(air,title='Average Causes of Air Pollution').mark_bar().encode(
-    x= 'Variable Name',
-    y= 'Value'
-)
-
-st.altair_chart(bar, use_container_width=True)
-
-
-#pollutants over time
-bar = alt.Chart(air, title = 'Pollutants Over Time').mark_line().encode(
-    x= 'Date',
-    y= 'Pollutant'
-)
-st.altair_chart(bar, use_container_width=True)
-
-#map
